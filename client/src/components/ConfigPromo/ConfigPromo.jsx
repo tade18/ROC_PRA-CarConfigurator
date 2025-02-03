3c6a827d2eededf5177d706b15949051bebb57c5
Tadeáš Keller <tadeaskeller@gmail.com>
2025-02-03 15:25:20 +0100

    added server file

--- client/src/components/ConfigPromo/ConfigPromo.jsx
+++ client/src/components/ConfigPromo/ConfigPromo.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import './ConfigPromo.css'
 import { Link } from 'react-router-dom'
 
 export default function ConfigPromo() {
   return (
     <>
         <div className="bg-yellow-300 flex items-center justify-between w-full h-20 pl-10">
-      <div className="text-black text-4xl font-bold">
+      <div className="text-black text-xl font-bold xl:text-5xl ">
         Nový vůz z pohodlí domova.
       </div>
-      <Link to={"/Configurator"}><button id='configPromo-button' className='mr-10'><span>Objevit</span></button></Link>
+      <Link to={"/Configurator"}><button id='configPromo-button' className='px-6 py-2 mr-10 bg-black text-yellow-300 font-bold rounded-md border-2 border-black hover:bg-yellow-300 hover:text-black transition'><span>Objevit</span></button></Link>
     </div>
     </>
   )
